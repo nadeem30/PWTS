4b6bdf27fe499d4fa520d69c56912be36f24bdfb
Nadeem <ahmednadeem4ever@gmail.com>
2026-08-09 21:31:53 +0530

    Aug 9 Commit 3 Updated- LearnLocators test

--- tests/LearnLocators.spec.ts
+++ tests/LearnLocators.spec.ts
@@ -8,18 +8,18 @@ test('Check Locators', async({page})=>{
 
 
 
-// await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-// await page.getByPlaceholder('Username').fill('Admin');
-// await page.getByPlaceholder('Password').fill('admin123');
-// await page.getByRole('button', {name: 'Login'}).click();
+await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+await page.getByPlaceholder('Username').fill('Admin');
+await page.getByPlaceholder('Password').fill('admin123');
+await page.getByRole('button', {name: 'Login'}).click();
 
-// await page.goto('https://www.testmuai.com/selenium-playground/input-form-demo/');
-// await page.getByTitle('Hello, have a question? Let’s chat.').click();
+await page.goto('https://www.testmuai.com/selenium-playground/input-form-demo/');
+await page.getByTitle('Hello, have a question? Let’s chat.').click();
 
-await page.goto('https://testautomationpractice.blogspot.com/p/playwrightpractice.html');
-await page.getByAltText('logo image').isVisible();
+// await page.goto('https://testautomationpractice.blogspot.com/p/playwrightpractice.html');
+// await page.getByAltText('logo image').isVisible();
 
 
-await page.waitForTimeout(5000);
+await page.waitForTimeout(7000);
 
 })  
