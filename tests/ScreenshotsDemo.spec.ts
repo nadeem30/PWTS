978d8b5f966450de72e6f59d09cdc52543fcd758
Nadeem <ahmednadeem4ever@gmail.com>
2026-08-13 21:18:18 +0530

    August13_ScreenshotsDemo test

---
+++ tests/ScreenshotsDemo.spec.ts
@@ -0,0 +1,38 @@
+// 08 - Playwright With TypeScript - How to take screenshot
+// ===================================================================
+
+// In Playwright, taking screenshots is very simple and powerful.
+// use .screenshot() method
+
+// You can capture:
+
+// The viewport(visible part of the page)
+// await page.screenshot({ path: 'viewport.png'});
+
+// The entire page
+// await page.screenshot({ path: 'fullpage.png', fullPage: true });
+
+// A specific element
+// const ele = await page.locator("//h1")
+// await ele.screenshot({path: 'ele.png'})
+
+
+import {test, expect} from '@playwright/test';
+
+test('Screenshot', async ({page})=>{
+
+await page.goto('https://www.hackerrank.com/home');
+await page.waitForTimeout(5000);
+
+//viewport
+//await page.screenshot({path: 'C:/Playwright/PWTS/screenshots/HKviewport.png'});
+
+//fullpage
+//await page.screenshot({path: 'C:/Playwright/PWTS/screenshots/HKfullpage.png', fullPage:true});
+
+//Element Screenshot
+const ele= await page.getByAltText('HackerRank logo with the wordmark "HackerRank" and a green rectangle after "Rank"');
+await ele.screenshot({path: 'C:/Playwright/PWTS/screenshots/HKelement.png'});
+
+
+})
