07b6a5338d76c0bfe274075af2c88c969812c8c7
Nadeem <ahmednadeem4ever@gmail.com>
2026-08-16 14:34:02 +0530

    Added waitforTimeout for popup to appear

--- tests/August13Practice1.spec.ts
+++ tests/August13Practice1.spec.ts
@@ -3,18 +3,18 @@ import {test, expect} from '@playwright/test';
 test('screenshot test', async ({page})=>{
 
   await page.goto('https://www.cricinfo.com/series/caribbean-premier-league-2026-1534175/jamaica-kingsmen-vs-guyana-amazon-warriors-6th-match-1534184/match-report');
+  await page.waitForTimeout(7000);
+  page.on('dialog', async dialog=>{
+  console.log(await dialog.message());  
+  await dialog.accept();
+  })
 
-//   page.on('dialog', async dialog=>{
-//   console.log(await dialog.message());  
-//   await dialog.accept();
-//   })
+await page.getByText('Accept All').click();
 
-//await page.getByText('Accept All').click();
-
-const acceptBtn = page.getByText('Accept All', { exact: false });
-if (await acceptBtn.isVisible({ timeout: 5000 }).catch(() => false)) {
-  await acceptBtn.click();
-}
+// const acceptBtn = page.getByText('Accept All', { exact: false });
+// if (await acceptBtn.isVisible({ timeout: 5000 }).catch(() => false)) {
+//   await acceptBtn.click();
+// }
 const ele=page.getByAltText('Mohammad Nabi finished with four wickets, Jamaica Kingsmen vs Guyana Amazon Warriors, CPL 2026, Kington, August 13, 2026');
 await ele.screenshot({path: 'nabi.png'});
 
