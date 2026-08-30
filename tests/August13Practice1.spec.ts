76528ca3f76615f82618482311b50b2cb1a39456
Nadeem <ahmednadeem4ever@gmail.com>
2026-08-30 10:47:47 +0530

    modified tests/August13Practice1.spec.ts

--- tests/August13Practice1.spec.ts
+++ tests/August13Practice1.spec.ts
@@ -2,7 +2,7 @@ import {test, expect} from '@playwright/test';
 
 test('screenshot test', async ({page})=>{
 
-  await page.goto('https://www.cricinfo.com/series/caribbean-premier-league-2026-1534175/jamaica-kingsmen-vs-guyana-amazon-warriors-6th-match-1534184/match-report');
+  await page.goto('https://www.pokemon.com/us');
   await page.waitForTimeout(7000);
   page.on('dialog', async dialog=>{
   console.log(await dialog.message());  
@@ -15,8 +15,8 @@ const acceptBtn = page.getByText('Accept All', { exact: false });
 if (await acceptBtn.isVisible({ timeout: 5000 }).catch(() => false)) {
   await acceptBtn.click();
 }
-const ele=page.getByAltText('Mohammad Nabi finished with four wickets, Jamaica Kingsmen vs Guyana Amazon Warriors, CPL 2026, Kington, August 13, 2026');
-await ele.screenshot({path: 'nabi.png'});
+const ele=page.getByAltText('Watch PokémonXP Live');
+await ele.screenshot({path: 'PokémonXP.png'});
 
 
 
